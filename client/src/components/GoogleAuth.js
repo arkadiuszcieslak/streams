49446a4e266ea77e07a183b6c81106b5981d9add
Arkadiusz Cieslak <arekc70@gmail.com>
2019-11-17 16:51:00 +0100

    Added singIn -> signOut listener.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '577328116033-g1j17jg1rilmi2k57cl1pn744r40jei6.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChanged);
       });
     });
   }
 
+  onAuthChanged = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I'm signed in</div>;
+    } else {
+      return <div>I'm NOT signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
