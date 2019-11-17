74f616222a80b0885ace669bf556064cc9487d8f
Arkadiusz Cieslak <arekc70@gmail.com>
2019-11-17 16:21:02 +0100

    Initiated google OAuth2.

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '577328116033-g1j17jg1rilmi2k57cl1pn744r40jei6.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
