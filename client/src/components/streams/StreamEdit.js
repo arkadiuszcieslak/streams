047a7a0ea362337cd7d43da65dfe8b9b4f746baf
Cieslak <e-azck@gft.com>
2019-12-18 15:14:32 +0100

    Started building StreamEdit page.

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return "Loading...";
+    }
+    return (
+      <div>
+        <h2>Stream</h2>
+        <div className="ui celled list">
+          <i className="large middle aligned icon camera"/>
+          <div className="content">
+            {this.props.stream.title}
+          </div>
+          <div className="description">
+            {this.props.stream.description}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
